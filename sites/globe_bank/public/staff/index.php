85979f913b51386bee57a08cdff06c3cf8a7dc2b
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2018-02-24 21:08:44 +1100

    First commit

---
+++ sites/globe_bank/public/staff/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>GBI</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../stylesheets/staff.css">
+  </head>
+
+  <body>
+
+<header>
+<h1>GBI Staff area</h1>
+</header>
+
+<navigation>
+<ul>
+  <li><a href="index.php">Menu</a></li>
+</ul>
+</navigation>
+
+<footer>
+&copy; <?php echo date('Y'); ?> Globe Bank 
+</footer>
+
+</body>
+</html>
